758e98b89d006a632c3d6161cc5c8d651056b97b
MariaRipley <roblesb.maria@gmail.com>
2024-01-19 19:46:55 +0100

    Update CRUD after checking with Thunder Task

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -2,7 +2,9 @@ import Task from "../models/tasks.model.js";
 
 //Traer todo
 export const getTasks = async (req, res) => {
-  const tasks = await Task.find();
+  const tasks = await Task.find({
+    user: req.user.id,
+  }).populate("user");
   res.json(tasks);
 };
 
@@ -13,6 +15,7 @@ export const createTask = async (req, res) => {
     title,
     description,
     date,
+    user: req.user.id,
   });
   const savedTask = await newTask.save();
   res.json(savedTask);
