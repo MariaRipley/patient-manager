fd2e0f878418d979d5589a2c35f40e217ac58012
MariaRipley <roblesb.maria@gmail.com>
2024-01-19 17:38:28 +0100

    Add login function

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -33,4 +33,36 @@ export const register = async (req, res) => {
   }
 };
 
-export const login = (req, res) => res.send("login");
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    //Buscar usuario por email
+    const userFound = await User.findOne({ email });
+
+    //Error si no se encuentra el usuario
+    if (!userFound) return res.status(400).json({ message: "User not found" });
+
+    //Comparar contraseña guardada con la que pone el usuario
+    const isMatch = await bcrypt.compare(password, userFound.password);
+
+    //Si no coincide:
+    if (!isMatch) return res.status(400).json({ message: "Icorrect password" });
+
+    //Si coincide:
+    //Crear y obtener el token
+    const token = await createAccessToken({ id: userFound._id });
+    //Guardar token en una cookie
+    res.cookie("token", token);
+    //Responder al frontend los datos de usuario y el token
+    res.json({
+      id: userFound._id,
+      username: userFound.username,
+      email: userFound.email,
+      createdAt: userFound.createdAt,
+      updatedAt: userFound.updatedAt,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
