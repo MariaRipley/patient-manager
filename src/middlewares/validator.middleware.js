f94c41205279b16eedd758312cb9699dab75a896
MariaRipley <roblesb.maria@gmail.com>
2024-01-22 18:11:28 +0100

    Add taskts validations

--- src/middlewares/validator.middleware.js
+++ src/middlewares/validator.middleware.js
@@ -1,9 +1,11 @@
 //Middleware para validar
-const validateSchema = (schema) => (req, res, next) => {
+export const validateSchema = (schema) => (req, res, next) => {
   try {
     schema.parse(req.body);
     next();
   } catch (error) {
-    return res.status(400).json({ error });
+    return res
+      .status(400)
+      .json({ error: error.errors.map((error) => error.message) });
   }
 };
