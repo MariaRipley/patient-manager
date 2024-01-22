f482e61fda2e99ec07961b68e373b5d330be27bd
MariaRipley <roblesb.maria@gmail.com>
2024-01-22 17:53:55 +0100

    Add validator middleware

---
+++ src/middlewares/validator.middleware.js
@@ -0,0 +1,9 @@
+//Middleware para validar
+const validateSchema = (schema) => (req, res, next) => {
+  try {
+    schema.parse(req.body);
+    next();
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+};
