37c5d7920eeabb1393f8d8dcdbca9c5a49f4f111
MariaRipley <roblesb.maria@gmail.com>
2024-01-23 19:17:35 +0100

    Add rest of the pages routes to App.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,9 @@ import { AuthProvider } from "./context/AuthContext.jsx";
 
 import RegisterPage from "./pages/RegisterPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
+import TasksPage from "./pages/TasksPage.jsx";
+import TaskFormPage from "./pages/TaskFormPage.jsx";
+import ProfilePage from "./pages/ProfilePage.jsx";
 
 function App() {
   return (
@@ -12,10 +15,10 @@ function App() {
           <Route path="/" element={<h1>Home page</h1>}></Route>
           <Route path="/login" element={<LoginPage />}></Route>
           <Route path="/register" element={<RegisterPage />}></Route>
-          <Route path="/tasks" element={<h1>Tasks page</h1>}></Route>
-          <Route path="/add-task" element={<h1>New task</h1>}></Route>
-          <Route path="/tasks/:id" element={<h1>Update task</h1>}></Route>
-          <Route path="/profile" element={<h1>Profile</h1>}></Route>
+          <Route path="/tasks" element={<TasksPage />}></Route>
+          <Route path="/add-task" element={<TaskFormPage />}></Route>
+          <Route path="/tasks/:id" element={<TaskFormPage />}></Route>
+          <Route path="/profile" element={<ProfilePage />}></Route>
         </Routes>
       </BrowserRouter>
     </AuthProvider>
