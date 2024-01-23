e26368dd5dd2abac8752528f26f13987ab8ea1b2
MariaRipley <roblesb.maria@gmail.com>
2024-01-23 19:10:01 +0100

    Fix format issue within sigin function

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -35,7 +35,10 @@ export const AuthProvider = ({ children }) => {
       const res = await loginRequest(user);
       console.log(res);
     } catch (error) {
-      setErrors(error.response.data);
+      if (Array.isArray(error.response.data)) {
+        setErrors(error.response.data);
+      }
+      setErrors([error.response.data.message]);
     }
   };
 
