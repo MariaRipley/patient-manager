a1b767c59ab593c41b7c3f698f7a5b5960f505dc
MariaRipley <roblesb.maria@gmail.com>
2024-01-23 17:12:31 +0100

    Add user context and error messages to form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext.jsx";
+
 import RegisterPage from "./pages/RegisterPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<h1>Home page</h1>}></Route>
-        <Route path="/login" element={<LoginPage />}></Route>
-        <Route path="/register" element={<RegisterPage />}></Route>
-        <Route path="/tasks" element={<h1>Tasks page</h1>}></Route>
-        <Route path="/add-task" element={<h1>New task</h1>}></Route>
-        <Route path="/tasks/:id" element={<h1>Update task</h1>}></Route>
-        <Route path="/profile" element={<h1>Profile</h1>}></Route>
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Home page</h1>}></Route>
+          <Route path="/login" element={<LoginPage />}></Route>
+          <Route path="/register" element={<RegisterPage />}></Route>
+          <Route path="/tasks" element={<h1>Tasks page</h1>}></Route>
+          <Route path="/add-task" element={<h1>New task</h1>}></Route>
+          <Route path="/tasks/:id" element={<h1>Update task</h1>}></Route>
+          <Route path="/profile" element={<h1>Profile</h1>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
