5e34a0460d32d532bf9e848880357fd62b1ff1e2
MariaRipley <roblesb.maria@gmail.com>
2024-01-23 13:12:48 +0100

    Create routes with react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Home page</h1>}></Route>
+        <Route path="/login" element={<h1>Login</h1>}></Route>
+        <Route path="/register" element={<h1>Register</h1>}></Route>
+        <Route path="/tasks" element={<h1>Tasks page</h1>}></Route>
+        <Route path="/add-task" element={<h1>New task</h1>}></Route>
+        <Route path="/tasks/:id" element={<h1>Update task</h1>}></Route>
+        <Route path="/profile" element={<h1>Profile</h1>}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
