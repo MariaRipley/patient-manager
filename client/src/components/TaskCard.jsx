9ab4931999422b2fe9cfc3211aa9dc5998902e5f
MariaRipley <roblesb.maria@gmail.com>
2024-02-26 17:52:17 +0100

    Add style to Delete and Edit card buttons

--- client/src/components/TaskCard.jsx
+++ client/src/components/TaskCard.jsx
@@ -11,18 +11,24 @@ function TaskCard({ task }) {
   const { deleteTask } = useTasks();
 
   return (
-    <div className="bg-zinc-800 p-10 rounded-md">
-      <header className="flex justify-between flex-wrap">
-        <h1 className="text-2xl font-bold flex-grow">{task.title}</h1>
-        <div className="flex flex-wrap gap-x-2 items-center">
+    <div className="bg-zinc-800 max-w-md w-full p-10 rounded-md">
+      <header className="flex justify-between">
+        <h1 className="text-2xl font-bold">{task.title}</h1>
+        <div className="flex gap-x-2 items-center">
           <button
+            className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-md"
             onClick={() => {
               deleteTask(task._id);
             }}
           >
             Delete
           </button>
-          <Link to={`/tasks/${task._id}`}>Edit</Link>
+          <Link
+            to={`/tasks/${task._id}`}
+            className="bg-blue-500 hover: bg-blue-600 text-white px-4 py-2 rounded-md"
+          >
+            Edit
+          </Link>
         </div>
       </header>
       <p className="text-slate-300">{task.description}</p>
